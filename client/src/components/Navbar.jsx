b2438cd81c3ba4c7a9c8998e8897d0a94afad34c
jacquesjg <jjeangi@gmail.com>
2021-12-29 13:46:23 +0100

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex md:justify-center'>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
